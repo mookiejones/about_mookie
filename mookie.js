2297308c4a908026a925fd5a09a0646c90ee6275
mookie <charles.heath.berman@gmail.com>
2015-02-18 20:58:58 -0500

    updated

--- mookie.js
+++ mookie.js
@@ -1,40 +1,56 @@
 
 
-
-
-
-
 if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault('counter', 0);
-Session.setDefault('loaded', false);
+  Session.setDefault('loaded', false);
+
+
+  $("a[href^=#]").on("click", function(event) {
+    event.preventDefault();
+    var $link = $(this);
+    var $target = $($link.attr("href"));
+    $("html,body").animate({
+      scrollTop: $target.offset().top
+    }, 660);
+  });
+
+  setTimeout(function() {
+    $("body").removeClass("is-loading").addClass("is-loaded");
+    $(".pre-loader").fadeOut();
+    $window.trigger('scroll');
+  }, 800);
+
+
+  var $window = $(window);
+  $window.on("scroll", function() {
+    $("section").each(function(index, section) {
+      var $section = $(section);
+      var sectionTop = $section.offset().top;
+      var sectionBottom = $section.offset().top + $section.height();
+      if ($window.scrollTop() >= sectionTop && $window.scrollTop() < sectionBottom) {
+        $section.addClass('current');
+      } else {
+        $section.removeClass('current').removeClass('animate');
+      }
+    });
+
+    $("section:in-viewport(200)").do(function() {
+	console.log('animating');
+      $(this).addClass('animate');
+      var $navItem = $("nav a[href=#" + $(this).attr("id") + "]");
+      $('nav a').removeClass('current');
+      $navItem.addClass('current');
+    });
+  });
+
 
 
 
 
   Template.appBody.rendered=function(){
     window.scrollReveal = new scrollReveal();
-     var $window = $(window);
-    $window.on("scroll", function() {
-      $("section").each(function(index, section) {
-        var $section = $(section);
-        var sectionTop = $section.offset().top;
-        var sectionBottom = $section.offset().top + $section.height();
-        if ($window.scrollTop() >= sectionTop && $window.scrollTop() < sectionBottom) {
-          $section.addClass('current');
-        } else {
-          $section.removeClass('current').removeClass('animate');
-        }
-    });
-
-    $("section:in-viewport(200)").do(function() {
-/*      $(this).addClass('animate');
-      var $navItem = $("nav a[href=#" + $(this).attr("id") + "]");
-      $('nav a').removeClass('current');
-      $navItem.addClass('current');*/
-    });
-
-  });
+    
     window.setTimeout(function(){
       Router.go('home');
     },800);
